ad855881b863140a124f8f69f1d820a0f4434635
Rafael Leite <costtaraphael@gmail.com>
2023-09-26 09:33:13 -0300

    cinema

--- tamagochi-java/Main.java
+++ tamagochi-java/Main.java
@@ -4,14 +4,14 @@
 
 import java.util.Scanner;
 
-class Pet{
+class Pet {
     private int energyMax, hungryMax, cleanMax;
     private int energy, hungry, clean;
     private int diamonds;
     private int age;
     private boolean alive;
 
-    public Pet(int energy, int hungry, int clean){
+    public Pet(int energy, int hungry, int clean) {
         this.energyMax = energy;
         this.hungryMax = hungry;
         this.cleanMax = clean;
@@ -23,60 +23,55 @@ class Pet{
         this.energy = energy;
         this.hungry = hungry;
         this.clean = clean;
-        
+
     }
 
-    void setEnergy(int value){
-        if(value <= 0){
+    void setEnergy(int value) {
+        if (value <= 0) {
             this.energy = 0;
             System.out.println("fail: pet morreu de fraqueza");
             this.alive = false;
             return;
-        } 
-        if(value > this.energyMax) {
+        }
+        if (value > this.energyMax) {
             this.energy = this.energyMax;
             return;
-        }
-        else{
+        } else {
             this.energy = value;
         }
 
     }
 
-
-    public void setHungry(int value){
-        if(value <= 0){
+    public void setHungry(int value) {
+        if (value <= 0) {
             this.hungry = 0;
             System.out.println("fail: pet morreu de fome");
             this.alive = false;
             return;
-        } 
-        if(value > this.hungryMax) {
+        }
+        if (value > this.hungryMax) {
             this.hungry = this.hungryMax;
             return;
-        }
-        else{
+        } else {
             this.hungry = value;
         }
     }
-    
-    void setClean(int value){
 
-        if(value <= 0){
+    void setClean(int value) {
+
+        if (value <= 0) {
             this.clean = 0;
             System.out.println("fail: pet morreu de sujeira");
             this.alive = false;
             return;
-        } 
-        if(value > this.cleanMax) {
+        }
+        if (value > this.cleanMax) {
             this.clean = this.cleanMax;
             return;
-        }
-        else{
+        } else {
             this.clean = value;
         }
     }
-    
 
     private boolean testAlive() {
         if (this.alive) {
@@ -86,13 +81,12 @@ class Pet{
         }
     }
 
-
-    public String toString(){
+    public String toString() {
         String ss = "";
-        ss +=  "E:" + energy + "/" + energyMax + ", "
-            +  "S:" + hungry + "/" + hungryMax + ", "
-            +  "L:" + clean + "/" + cleanMax + ", "
-            +  "D:" + diamonds + ", " + "I:"  + age;
+        ss += "E:" + energy + "/" + energyMax + ", "
+                + "S:" + hungry + "/" + hungryMax + ", "
+                + "L:" + clean + "/" + cleanMax + ", "
+                + "D:" + diamonds + ", " + "I:" + age;
         return ss;
     }
 
@@ -106,92 +100,108 @@ class Pet{
         diamonds += 1;
         age += 1;
     }
-    
-    public void shower(){
-        if(!this.testAlive())
+
+    public void shower() {
+        if (!this.testAlive())
             return;
         setEnergy(getEnergy() - 3);
         setHungry(getHungry() - 1);
         setClean(this.cleanMax);
         this.diamonds += 0;
         this.age += 2;
-        
+
     }
 
-    public void eat(){
-        if(!this.testAlive())
+    public void eat() {
+        if (!this.testAlive())
             return;
         setEnergy(getEnergy() - 1);
         setHungry(getHungry() + 4);
         setClean(getClean() - 2);
         this.diamonds += 0;
         this.age += 1;
-        
-        
+
     }
 
-    public void sleep(){
-        if(!this.testAlive())
+    public void sleep() {
+        if (!this.testAlive())
             return;
 
-        if(getEnergy() <= getEnergyMax() - 5){
+        if (getEnergy() <= getEnergyMax() - 5) {
             this.age += getEnergyMax() - getEnergy();
             setEnergy(this.energyMax);
             setHungry(getHungry() - 1);
-        }
-        else{
+        } else {
             System.out.println("fail: nao esta com sono");
         }
-            
-        
+
     }
 
-
-    int getClean(){
+    int getClean() {
         return this.clean;
     }
-    int getHungry(){
+
+    int getHungry() {
         return this.hungry;
     }
-    int getEnergy(){
+
+    int getEnergy() {
         return this.energy;
     }
-    int getEnergyMax(){
+
+    int getEnergyMax() {
         return this.energyMax;
     }
-    int getCleanMax(){
+
+    int getCleanMax() {
         return this.cleanMax;
     }
-    int getHungryMax(){
+
+    int getHungryMax() {
         return this.hungryMax;
     }
 }
 
-
-
-
-public class Main {
+class Main {
     public static void main(String[] a) {
         Pet pet = new Pet(0, 0, 0);
-        
+
         while (true) {
             var line = input();
             write("$" + line);
             var args = line.split(" ");
 
-            if      (args[0].equals("end"))   { break;                                                                           }
-            else if (args[0].equals("show"))  { write(pet.toString());                                                           }
-            else if (args[0].equals("init"))  { pet = new Pet((int)number(args[1]), (int)number(args[2]), (int)number(args[3])); }
-            else if (args[0].equals("play"))  { pet.play();                                                                      }
-            else if (args[0].equals("eat"))   { pet.eat();                                                                       }
-            else if (args[0].equals("sleep")) { pet.sleep();                                                                     }
-            else if (args[0].equals("shower")){ pet.shower();                                                                    }
-            else                              { write("fail: comando invalido");                                                 }
+            if (args[0].equals("end")) {
+                break;
+            } else if (args[0].equals("show")) {
+                write(pet.toString());
+            } else if (args[0].equals("init")) {
+                pet = new Pet((int) number(args[1]), (int) number(args[2]), (int) number(args[3]));
+            } else if (args[0].equals("play")) {
+                pet.play();
+            } else if (args[0].equals("eat")) {
+                pet.eat();
+            } else if (args[0].equals("sleep")) {
+                pet.sleep();
+            } else if (args[0].equals("shower")) {
+                pet.shower();
+            } else {
+                write("fail: comando invalido");
+            }
         }
     }
 
     private static Scanner scanner = new Scanner(System.in);
-    private static String  input()              { return scanner.nextLine(); }
-    private static double  number(String value) { return Double.parseDouble(value); }
-    private static void    write(String value)  { System.out.println(value); }
+
+    private static String input() {
+        return scanner.nextLine();
+    }
+
+    private static double number(String value) {
+        return Double.parseDouble(value);
+    }
+
+    private static void write(String value) {
+        System.out.println(value);
+    }
 }
